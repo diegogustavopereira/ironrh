7d6ccd41ed5754c97d16a4bafb3547ca0fb6077a
tatialveso <tatiialveso@gmail.com>
2022-11-17 12:22:50 -0300

    :sparkles: create search

--- src/components/People/PeopleList/PeopleList.js
+++ src/components/People/PeopleList/PeopleList.js
@@ -1,11 +1,12 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { Button, Container, Spinner, Table } from "react-bootstrap"
+import { Button, Container, Form, Spinner, Table } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 function PeopleList({ apiURL }) {
     const [employees, setEmployees] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [search, setSearch] = useState("")
 
     useEffect(() => {
         try {
@@ -21,24 +22,34 @@ function PeopleList({ apiURL }) {
         }
     }, [])
 
-    const renderEmployees = employees.map((employee) => {
-        return (
-            <tr key={employee._id}>
-                <td>{employee.name}</td>
-                <td>{employee.email}</td>
-                <td>{employee.department}</td>
-                <td>{employee.status}</td>
-                <td>
-                    <Button variant="info" size="sm">
-                        <Link className="nav-link" to={`/funcionarios/${employee._id}`}>Ver detalhes</Link>
-                    </Button>
-                </td>
-            </tr>
-        )
-    })
+    const renderEmployees = employees
+        .filter((employee) => employee.name.toLowerCase().includes(search.toLowerCase()))
+        .map((employee) => {
+            return (
+                <tr key={employee._id}>
+                    <td>{employee.name}</td>
+                    <td>{employee.email}</td>
+                    <td>{employee.department}</td>
+                    <td>{employee.status}</td>
+                    <td>
+                        <Button variant="info" size="sm">
+                            <Link className="nav-link" to={`/funcionarios/${employee._id}`}>Ver detalhes</Link>
+                        </Button>
+                    </td>
+                </tr>
+            )
+        })
 
     return (
         <Container>
+            <Form className="my-4">
+                <Form.Control
+                    type="search"
+                    placeholder="Procurar funcionário"
+                    value={ search }
+                    onChange={ (e) => setSearch(e.target.value) }
+                />
+            </Form>
             {isLoading && <Spinner className="" animation="border" />}
             {!isLoading &&
                 <Table className="mt-4" striped bordered hover>
